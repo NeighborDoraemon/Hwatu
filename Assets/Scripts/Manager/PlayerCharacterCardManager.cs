97e59a05486e566184b1b9132ab9a4888dc9e1e6
NeighborDoraemon <boyoung8260@naver.com>
2024-09-04 21:10:47 +0900

    캐릭터 컨트롤 추가
    순간이동, 더블점프, 원거리공격 추가

--- Assets/Scripts/Manager/PlayerCharacterCardManager.cs
+++ Assets/Scripts/Manager/PlayerCharacterCardManager.cs
@@ -7,9 +7,9 @@ public class PlayerCharacterCardManager : PlayerCharacterStatManager
     public CardUIManager cardUIManager; // 화투 UI 매니저 변수
 
     public GameObject[] cardInventory = new GameObject[2]; // 화투 오브젝트 저장 공간 배열
-    public int cardCount = 0; // 화투 갯수 카운트 변수
+    protected int cardCount = 0; // 화투 갯수 카운트 변수
 
-    public bool isCombDone = false; // 화투 조합이 이루어졌는지 체크하는 변수
+    protected bool isCombDone = false; // 화투 조합이 이루어졌는지 체크하는 변수
 
     public void AddCard(GameObject card) // 화투 저장 함수
     {
@@ -52,21 +52,21 @@ public class PlayerCharacterCardManager : PlayerCharacterStatManager
             string card_2_Name = cardInventory[1].name;
 
             if ((card_1_Name == "Card_1M" && card_2_Name == "Card_2M") ||
-                    (card_2_Name == "Card_2M" && card_1_Name == "Card_1M"))
+                    (card_1_Name == "Card_2M" && card_2_Name == "Card_1M"))
             {
                 movementSpeed += 5;
                 Debug.Log("이동속도 증가");
                 isCombDone = true;
             }
             else if ((card_1_Name == "Card_1M" && card_2_Name == "Card_3M") ||
-                    (card_2_Name == "Card_3M" && card_1_Name == "Card_1M"))
+                    (card_1_Name == "Card_3M" && card_2_Name == "Card_1M"))
             {
                 jumpPower += 5;
                 Debug.Log("점프력 증가");
                 isCombDone = true;
             }
             else if ((card_1_Name == "Card_2M" && card_2_Name == "Card_3M") ||
-                    (card_2_Name == "Card_3M" && card_1_Name == "Card_2M"))
+                    (card_1_Name == "Card_3M" && card_2_Name == "Card_2M"))
             {
                 movementSpeed += 2;
                 jumpPower += 2;
--- Assets/Scripts/PlayerCharacter/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacter/PlayerCharacterController.cs
@@ -8,13 +8,18 @@ public class PlayerCharacterController : PlayerCharacterCardManager
     Rigidbody2D rb;
 
     Vector2 movement = new Vector2();
-    bool isJumping = false;
+    int jumpCount = 0; // 점프 횟수 카운팅 변수
+    public int maxJumpCount = 2; // 최대 점프 횟수 카운팅 변수
 
-    GameObject currentItem;
+    public float teleportingDistance = 3.0f; // 순간이동 거리 변수
+    public float teleportingCoolTime = 3.0f; // 순간이동 쿨타임 변수
+    bool canTeleporting = true; // 순간이동 조건 확인 변수
 
+    GameObject currentItem; // 현재 아이템 확인 변수
 
-    [SerializeField] // 맵 매니저 #김윤혁
-    private Map_Manager map_Manager;
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float projectileSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +30,16 @@ public class PlayerCharacterController : PlayerCharacterCardManager
     // Update is called once per frame
     void Update()
     {
-        Move();
+        Move(); // 이동
         Jump();
+        Teleportation();
 
-        InterAction();
+        InterAction(); // 상호작용
 
-        if (!isCombDone)
+        LongRangeAttack(); // 공격
+        ShortRangeAttack();
+
+        if (!isCombDone) // 카드 조합 확인 조건
         {
             CardCombination();
         }
@@ -45,17 +54,22 @@ public class PlayerCharacterController : PlayerCharacterCardManager
     {
         movement.x = Input.GetAxisRaw("Horizontal");
 
-        movement.Normalize();
+        if (movement.x != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(movement.x), 1, 1);
+        }
 
+        movement.Normalize();
         rb.velocity = new Vector2(movement.x * movementSpeed, rb.velocity.y);
     }
 
     void Jump() // 캐릭터 y좌표 점프
     {
-        if (Input.GetButtonDown("Jump") && isJumping == false)
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumpCount)
         {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
-            isJumping = true;
+            jumpCount++;
             //Debug.Log("점프");
         }
     }
@@ -79,11 +93,61 @@ public class PlayerCharacterController : PlayerCharacterCardManager
         }
     }
 
+    void Teleportation() // 플레이어 캐릭터 순간이동 함수
+    {
+        if (Input.GetKeyDown(KeyCode.D) && canTeleporting)
+        {
+            if (movement.x < 0)
+            {
+                transform.Translate(Vector2.left * teleportingDistance);
+            }
+            else
+            {
+                transform.Translate(Vector2.right * teleportingDistance);
+            }
+            canTeleporting = false;
+        }
+
+        if (!canTeleporting)
+        {
+            teleportingCoolTime -= Time.deltaTime;
+            if (teleportingCoolTime <= 0.0f)
+            {
+                teleportingCoolTime = 3.0f;
+                canTeleporting = true;
+                Debug.Log("순간이동 가능");
+            }
+        }
+    }
+
+    void ShortRangeAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+    }
+
+    void LongRangeAttack() // 원거리 공격 함수
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+
+            Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
+
+            Vector2 shootDirection = (transform.localScale.x < 0) ? Vector2.left : Vector2.right;
+            projectileRb.velocity = shootDirection * projectileSpeed;
+
+            Destroy(projectile, 5.0f);
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Platform")
         {
-            isJumping = false;
+            jumpCount = 0;
         }
     }
 
@@ -93,22 +157,6 @@ public class PlayerCharacterController : PlayerCharacterCardManager
         {
             //Debug.Log("아이템 감지");
             currentItem = other.gameObject;
-        }
-
-        if (other.gameObject.tag == "Portal" && map_Manager.IsOnPortal == false) // 포탈 진입 감지 #김윤혁
-        {
-            Debug.Log("Get In Portal");
-            map_Manager.IsOnPortal = true;
-            map_Manager.Which_Portal = other.gameObject;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other) // 포탈 퇴장감지 #김윤혁
-    {
-        if (other.gameObject.tag == "Portal" && map_Manager.IsOnPortal == true)
-        {
-            map_Manager.IsOnPortal = false;
-            Debug.Log("Get Out Portal");
         }
     }
 }
