82e854e20521698a8b767059323d50b53392ae76
colinxxx <70803729+colinxxx@users.noreply.github.com>
2025-03-22 20:49:48 +0900

    03.22 임시커밋

---
+++ Assets/Scripts/Controller/Enemies/Enemy_Nodes/BH_Stunned.cs
@@ -0,0 +1,22 @@
+using MBT;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("")]
+[MBTNode("Enemy/BH_Stunned")]
+
+public class BH_Stunned : Service
+{
+    public override void Task()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    //[SerializeField] 
+
+    public override void OnEnter()
+    {
+
+    }
+}
